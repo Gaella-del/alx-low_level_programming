c98ae4d441c28ea7e6943fb3c93c84c6c559deef
Mulisa Uwera Gaella <mulisagaella@gmail.com>
2021-06-24 06:06:34 -0700

    Added this file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+* File: 2-mul.c
+* Auth: Mulisa Gaella
+*/
+
+#include "holberton.h"
+
+/**
+* mul - Multiples two integers.
+* @c: The first integer to be multiplied.
+* @d: The second integer to be multiplied.
+*
+* Return: The result of the multiplication.
+*/
+int mul(int c, int d)
+{
+return (c * d);
+}
