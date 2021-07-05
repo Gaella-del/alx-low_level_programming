0ac680d43ae5660bca3155d4b8a68ca8799f5012
Mulisa Uwera Gaella <mulisagaella@gmail.com>
2021-07-05 12:55:54 -0700

    Added these files

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+* _strstr - searches for substring.
+* @haystack: pointer to checked string
+* @needle: substring
+*
+* Return: a pointer to the first byte of the substring
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+unsigned int counter, i, checker;
+
+for (counter = 0; haystack[counter] != '\0' ; counter++)
+{
+checker = 0;
+for (i = 0; needle[i] != '\0'; i++)
+{
+if (haystack[counter + i] == needle[i])
+checker = 1;
+else
+checker = 0;
+}
+if (checker && needle[i] == '\0')
+return (haystack + counter);
+}
+return (NULL);
+}
